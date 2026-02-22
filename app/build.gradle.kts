b319f23544024743bd0f36f1b3fe579a537bcd86
skupie <114020227+skupie@users.noreply.github.com>
2026-02-22 06:23:26 +0600

    Add files via upload

---
+++ app/build.gradle.kts
@@ -0,0 +1,31 @@
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "top.smscloudapp.basic.student"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "top.smscloudapp.basic.student"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+}
