8cb6ad9291c3e201cfbdccb1c23ae6f746220517
skupie <114020227+skupie@users.noreply.github.com>
2026-02-22 06:22:35 +0600

    Add files via upload

--- app/src/main/java/top/smscloudapp/basic/student/MainActivity.kt
+++ app/src/main/java/top/smscloudapp/basic/student/MainActivity.kt
@@ -1,3 +1,44 @@
-app/src/main/AndroidManifest.xml
-app/src/main/res/layout/activity_main.xml
-app/build.gradle.kts
+
+package top.smscloudapp.basic.student
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val swipeRefresh = findViewById<SwipeRefreshLayout>(R.id.swipeRefresh)
+        webView = findViewById(R.id.webView)
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                swipeRefresh.isRefreshing = false
+            }
+        }
+
+        swipeRefresh.setOnRefreshListener {
+            webView.reload()
+        }
+
+        webView.loadUrl("https://students.basic.smscloudapp.top")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
