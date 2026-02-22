fa796fe6864f3842a9358331a55441379b1bc4e2
skupie <114020227+skupie@users.noreply.github.com>
2026-02-22 11:23:33 +0600

    Add RegisterDeviceRequest data class

---
+++ app/src/main/java/top/smscloudapp/basic/student/data/remote/dto/RegisterDeviceRequest.kt
@@ -0,0 +1,14 @@
+package top.smscloud.basic.student.data.remote.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class RegisterDeviceRequest(
+    @SerializedName("device_token")
+    val deviceToken: String,
+    @SerializedName("platform")
+    val platform: String = "android",
+    @SerializedName("device_name")
+    val deviceName: String,
+    @SerializedName("app_version")
+    val appVersion: String
+)
