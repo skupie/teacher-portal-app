73e6a39dd2074ed32ff132aa24ee9f3e18b4a9a3
skupie <114020227+skupie@users.noreply.github.com>
2026-02-22 11:22:29 +0600

    Create LoginRequest.kt

---
+++ app/src/main/java/top/smscloudapp/basic/student/data/remote/dto/LoginRequest.kt
@@ -0,0 +1,20 @@
+package top.smscloud.basic.student.data.remote.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class LoginRequest(
+    @SerializedName("login")
+    val login: String,
+    @SerializedName("password")
+    val password: String,
+    @SerializedName("role")
+    val role: String, // "teacher" or "student"
+    @SerializedName("device_name")
+    val deviceName: String,
+    @SerializedName("device_token")
+    val deviceToken: String?,
+    @SerializedName("platform")
+    val platform: String = "android",
+    @SerializedName("app_version")
+    val appVersion: String
+)
