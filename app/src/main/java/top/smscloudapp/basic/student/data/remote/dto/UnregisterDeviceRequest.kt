78a895ea31e035e00d522fcfe6be8b47334639be
skupie <114020227+skupie@users.noreply.github.com>
2026-02-25 20:12:37 +0600

    Delete app/src/main/java/top/smscloudapp/basic/student/data directory

--- app/src/main/java/top/smscloudapp/basic/student/data/remote/dto/UnregisterDeviceRequest.kt
+++
@@ -1,8 +0,0 @@
-package top.smscloudapp.basic.student.data.remote.dto
-
-import com.google.gson.annotations.SerializedName
-
-data class UnregisterDeviceRequest(
-    @SerializedName("device_token")
-    val deviceToken: String
-)
